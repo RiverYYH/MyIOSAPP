244acaa1c0016b5942b727811dd70736459df447
river <>
2018-03-12 14:29:54 +0800

    添加第三方库ReactiveCocoa

--- MyIOSAPP/MyIOSAPP/ViewController.m
+++ MyIOSAPP/MyIOSAPP/ViewController.m
@@ -5,7 +5,7 @@
 //  Created by river on 2017/12/28.
 //  Copyright © 2017年 river. All rights reserved.
 //
-
+#import <CoreText/CoreText.h>
 #import "ViewController.h"
 #import "ModelDesignViewController.h"
 #import "UpLoadImageViewController.h"
@@ -15,9 +15,41 @@
 @interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *myTableView;
 @property (nonatomic,strong) NSMutableArray * dataArray;
+//@property (nonatomic,strong) UIButton * barRightBtn;
 @end
 
 @implementation ViewController
+
+- (NSArray *)getLinesArrayOfStringInLabel:(UILabel *)label{
+    NSString *text = [label text];
+    UIFont *font = [label font];
+    CGRect rect = [label frame];
+    
+    CTFontRef myFont = CTFontCreateWithName(( CFStringRef)([font fontName]), [font pointSize], NULL);
+    NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc] initWithString:text];
+    [attStr addAttribute:(NSString *)kCTFontAttributeName value:(__bridge  id)myFont range:NSMakeRange(0, attStr.length)];
+    CFRelease(myFont);
+    CTFramesetterRef frameSetter = CTFramesetterCreateWithAttributedString(( CFAttributedStringRef)attStr);
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddRect(path, NULL, CGRectMake(0,0,rect.size.width,100000));
+    CTFrameRef frame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(0, 0), path, NULL);
+    NSArray *lines = ( NSArray *)CTFrameGetLines(frame);
+    NSMutableArray *linesArray = [[NSMutableArray alloc]init];
+    for (id line in lines) {
+        CTLineRef lineRef = (__bridge  CTLineRef )line;
+        CFRange lineRange = CTLineGetStringRange(lineRef);
+        NSRange range = NSMakeRange(lineRange.location, lineRange.length);
+        NSString *lineString = [text substringWithRange:range];
+        CFAttributedStringSetAttribute((CFMutableAttributedStringRef)attStr, lineRange, kCTKernAttributeName, (CFTypeRef)([NSNumber numberWithFloat:0.0]));
+        CFAttributedStringSetAttribute((CFMutableAttributedStringRef)attStr, lineRange, kCTKernAttributeName, (CFTypeRef)([NSNumber numberWithInt:0.0]));
+        [linesArray addObject:lineString];
+    }
+    
+    CGPathRelease(path);
+    CFRelease( frame );
+    CFRelease(frameSetter);
+    return (NSArray *)linesArray;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -25,6 +57,29 @@
     self.title = @"我的APP";
     self.dataArray = [NSMutableArray arrayWithObjects:@"设计模式",@"多张图片上传",@"多线程NSOperationQueue",@"App有版本更新时候打开出现的提示", nil];
     self.myTableView.tableFooterView = [[UIView alloc] init];
+    
+    
+//    self.barRightBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+//    self.barRightBtn.frame = CGRectMake(WIDTH-20, 30, 80, 30);
+//    [self.barRightBtn setTitle:@"停止扫描" forState:UIControlStateNormal];
+//    [self.barRightBtn setTitle:@"开始扫描" forState:UIControlStateSelected];
+//
+//    [self.barRightBtn setTitleColor:BLUECOLOR forState:UIControlStateNormal];
+//    self.barRightBtn.titleLabel.font = [UIFont systemFontOfSize:18];
+//    [[self.barRightBtn rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
+//        self.barRightBtn.selected = !self.barRightBtn.selected;
+//
+//        if (self.barRightBtn.selected) {
+//            [self endScanIbeacon];
+//        }else{
+//            [self startScanIbecan];
+//        }
+//    }];
+//
+//    UIBarButtonItem *barBtn = [[UIBarButtonItem alloc] initWithCustomView:self.barRightBtn];
+//    self.navigationItem.rightBarButtonItem = barBtn;
+//
+
 }
 
 
